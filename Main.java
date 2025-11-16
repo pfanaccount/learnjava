085470537bfe81cee548541ba75996ff6415f3cc
pfanaccount <benjiatori2000@gmail.com>
2025-11-16 20:32:48 +0800

    panera bread shanking

--- Main.java
+++ Main.java
@@ -5,16 +5,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String restaurantType;
+        String name;
+        String adjective1;
+        String verb1;
+        String adjective2;
 
-        double width = 0;
-        double height = 0;
+        System.out.print("Enter a restaurant name: ");
+        restaurantType = scanner.nextLine();
+        System.out.print("Enter a name: ");
+        name = scanner.nextLine();
+        System.out.print("Enter an adjective: ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter a verb: ");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter an adjective: ");
+        adjective2 = scanner.nextLine();
 
-        System.out.print("Enter width (cm): ");
-        width = scanner.nextDouble();
-        System.out.print("Enter height (cm): ");
-        height = scanner.nextDouble();
 
-        System.out.println("The rectangles area is " + (width * height) + " cm squared.");
+
+        System.out.println("\nToday I went to a " +  restaurantType + " restaurant.");
+        System.out.println("In the restaurant, I saw " + name + "." );
+        System.out.println(name + " was " + adjective1 + " and " + verb1 + ".");
+        System.out.println("I was " + adjective2 + "!");
 
 
         scanner.close();
