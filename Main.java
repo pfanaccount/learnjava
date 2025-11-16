18af6a0b0cb401b906bf403734e6b08495940474
pfanaccount <benjiatori2000@gmail.com>
2025-11-16 19:27:52 +0800

    basic printing

--- Main.java
+++ Main.java
@@ -1,3 +1,21 @@
 public class Main {
-    
+   
+    public static void main(String[] args) {
+
+        int age = 21;
+        double average = 57.91;
+        char grade = 'C';
+        boolean isPassing = true;
+        String studentName = "Big Badinky Bones";
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student Age: " + age);
+        System.out.println("Student Average: " + average);
+        System.out.println("Student Grade: " + grade);
+        
+        if (isPassing) { 
+            System.out.println("The student is passing.");
+        }
+        
+    }
 }
