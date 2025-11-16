6c36e3a5897a2df9d7ecdfa25982230e97896c96
pfanaccount <benjiatori2000@gmail.com>
2025-11-16 20:16:49 +0800

    updated scanners

--- Main.java
+++ Main.java
@@ -12,9 +12,17 @@ public class Main {
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
 
+        System.out.print("Did you shank someone in the leg at Panera bread? (true/false)");
+        boolean didShank = scanner.nextBoolean();
 
         System.out.println("Hello " + name);
         System.out.println("You are " + age + " years old.");
+        if (didShank) {
+            System.out.println("You are a wanted felon.");
+        }
+        else {
+            System.out.println("You are not a wanted felon.");
+        }
 
         scanner.close();
     }
