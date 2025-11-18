be521376f460cabc650f5ba1ba928eb715657d8e
pfanaccount <benjiatori2000@gmail.com>
2025-11-18 11:41:56 +0800

    compound interest calculator

--- Main.java
+++ Main.java
@@ -1,33 +1,29 @@
+import java.util.Scanner;
 
 public class Main {
    
     public static void main(String[] args) {
 
-        String name = "pfanaccount";
-        char firstLetter = 'p';
-        int age = 19;
-        double height = 193.1975;
-        boolean isEmployed = false;
-        double bankBalance = -10821.53;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Hello %s\n", name);
-        System.out.printf("Your name starts with a %c\n", firstLetter);
-        System.out.printf("You are %d years old\n", age);
-        System.out.printf("Your height is %.1f cm tall\n", height);
-        System.out.printf("Employed: %b\n", isEmployed);
+        double principalInvestment;
+        double interestRate;
+        int compoundsPerYear;
+        double years;
 
-        System.out.printf("You are %d years old and %,.1f cm tall\n", age, height);
-        System.out.printf("You have %,.2f dollars\n", bankBalance);
+        System.out.print("Enter the principal investment: $");
+        principalInvestment = scanner.nextDouble();
+        System.out.print("Enter the interest rate (%): ");
+        interestRate = (scanner.nextDouble() / 100.0);
+        System.out.print("Enter the number of times compounded per year: ");
+        compoundsPerYear = scanner.nextInt();
+        System.out.print("Enter the number of years: ");
+        years = scanner.nextDouble();
 
-        int id1 = 1;
-        int id2 = 23;
-        int id3 = 456;
-        int id4 = 7890;
-        System.out.printf("%4d\n", id1);
-        System.out.printf("%4d\n", id2);
-        System.out.printf("%4d\n", id3);
-        System.out.printf("%4d\n", id4);
+        double finalInterest = (principalInvestment)* Math.pow(1 + (interestRate / compoundsPerYear), (compoundsPerYear * years));
+        System.out.printf("Your final balance is $%.2f\n", finalInterest);
 
+        scanner.close();
         }
 
 }
