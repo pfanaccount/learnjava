9bff30809e5bcbdfdb7beaa9aca88e0607c89739
pfanaccount <benjiatori2000@gmail.com>
2025-11-18 09:08:15 +0800

    triangle calculator

--- Main.java
+++ Main.java
@@ -1,24 +1,21 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
    
     public static void main(String[] args) {
 
-        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        double length;
+        double height;
 
-        boolean isHeads;
+        System.out.print("Enter triangle height: ");
+        height = scanner.nextDouble();
+        System.out.print("Enter triangle length: ");
+        length = scanner.nextDouble();
 
-
-        isHeads = random.nextBoolean();
-
-        if (isHeads) {
-            System.out.println("It's heads!");
-        }
-        else {
-            System.out.println("It's tails!");
-        }
-
-
-
+        double hypotenuse = Math.sqrt(Math.pow(length, 2) + Math.pow(height, 2));
+        System.out.println(hypotenuse);
+        scanner.close();
     }
+
 }
