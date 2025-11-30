99d13e4789fb34c74b4ae338290ee031cb45f8d4
pfanaccount <benjiatori2000@gmail.com>
2025-12-01 03:21:33 +0800

    it bigger

--- Main.java
+++ Main.java
@@ -5,11 +5,10 @@ public class Main {
         int myInt = 7;
         double myDouble = 7.01;
 
-        double myIntAsDouble = myInt;
-        int myDoubleAsInt = (int) myDouble;
+        if (!(myInt > myDouble)) {
+            System.out.println("it bigger");
 
-        System.out.println(myIntAsDouble);
-        System.out.println(myDoubleAsInt);
+        }
         }
 
 }
