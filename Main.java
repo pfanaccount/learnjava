7bfb676ce11e75a8a0f72ec1b728f92d0a349024
pfanaccount <benjiatori2000@gmail.com>
2025-12-01 10:14:12 +0800

    nested ifs

--- Main.java
+++ Main.java
@@ -2,13 +2,32 @@ public class Main {
    
     public static void main(String[] args) {
 
-        int myInt = 7;
-        double myDouble = 7.01;
+        boolean isStudent = true;
+        boolean isSenior = false;
+        double price = 9.99;
 
-        if (!(myInt > myDouble)) {
-            System.out.println("it bigger");
-
+        if (isStudent) {
+            System.out.println("You get a student discount of 10%");
+            if(isSenior) {
+                System.out.println("You get a senior discount of 20%");
+                price *= 0.7;
+            }
+            else {
+                System.out.println("You are not a senior.");
+                price *= 0.9;
+            }
         }
+        else if (isSenior){
+            System.out.println("You are not a student.");
+            System.out.println("You get a senior discount of 20%");
+            price *= 0.8;
+        }
+        else {
+            System.out.println("You get no discount.");
+        }
+
+        System.out.printf("The price of your ticket is $%.2f\n", price);
+
         }
 
 }
