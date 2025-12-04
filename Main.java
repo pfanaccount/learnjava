11ded95eb649fd1d15ff670c58a79df67c8bfbb3
pfanaccount <benjiatori2000@gmail.com>
2025-12-04 21:45:06 +0800

    weight converter

--- Main.java
+++ Main.java
@@ -6,22 +6,25 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String email;
+        System.out.println("Press 1 for lbs to kgs");
+        System.out.println("Press 2 for kgs to lbs");
 
-        System.out.print("Enter your email address: ");
-        email = scanner.nextLine();
+        int convInt = scanner.nextInt();
 
-        if (email.contains("@")) {
-            String username = email.substring(0, email.indexOf("@"));
-            String domain = email.substring(email.indexOf("@") + 1);
-            System.out.println(username);
-            System.out.println(domain);
+        System.out.println("Enter the weight: ");
+        double weight = scanner.nextDouble();
+
+        if (convInt == 1) {
+            double newWeight = (weight / 2.205);
+            System.out.printf("%f pounds = %.2f kilograms.", weight, newWeight);
+        }
+        else if (convInt == 2) {
+            double newWeight = (weight * 2.205);
+            System.out.printf("%f kilograms = %.2f pounds.", weight, newWeight);
         }
         else {
-            System.out.println("You did not enter a valid email!");
+            System.out.println("Enter a valid conversion choice!");
         }
-
-
 
         scanner.close();
     }
